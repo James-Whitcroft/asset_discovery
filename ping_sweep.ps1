d95c75f250cc425939470682b378dc58eee53bbb
James <jw7241@rit.edu>
2018-10-07 18:58:03 -0400

    its slow but it works...

--- ping_sweep.ps1
+++ ping_sweep.ps1
@@ -7,9 +7,9 @@ function addr_to_decimal {
   % {
     $fin += [convert]::ToString($_, 2).PadLeft(8, '0')
   }
-  write-host $fin
   return [convert]::ToInt64($fin, 2)
 }
+
 
 function decimal_to_addr {
   Param($decimal)
@@ -27,6 +27,24 @@ function decimal_to_addr {
 
 
 function ping_range {
+  Param($addr, $range)
+  [string]$ping_to = $addr
+  $up_hosts = @()
+  for($x = 0; $x -lt $range; $x++) {
+    $ret = ping $ping_to -n 1 -w 50
+    <#write-host "Pinging host: " $ping_to#>
+    if(( -not ($ret -match "unreachable")) -and  (-not ($ret -match "timed out"))){
+      write-host "Host " $ping_to " UP"
+      $up_hosts += $ping_to
+    }
+    $ping_to = (addr_to_decimal -addr $ping_to) + 1
+    $ping_to = decimal_to_addr -decimal $ping_to
+  }
+  return $up_hosts
+}
+
+
+function parse_range {
   Param($range)
   
   $split_addr = @()
@@ -41,35 +59,37 @@ function ping_range {
   else {
     Write-Host ("Improperly formatted range: " + $range)
   }
-  write-host $split_addr
   return $split_addr
 }
-
-
-
 
 
 function main {
   $user_range = read-host "Enter IP range or CIDR notation >> "
   
-  $parsed = ping_range -range $user_range
+  $parsed = parse_range -range $user_range
   if($parsed[-1] -eq "range") {
     $start = addr_to_decimal -addr $parsed[0]
     $end = addr_to_decimal -addr $parsed[1]
     $range = $end - $start
-    write-host "Start: " $start " End: " $end " Range: " $range
+    $ping_to = decimal_to_addr -decimal $start
+    $up_hosts = ping_range -addr $ping_to -range $range
   }
   elseif($parsed[-1] -eq "cidr") {
     $start = addr_to_decimal -addr $parsed[0]
+    $start++
     $range = 1 -shl (32 - $parsed[1])
-    write-host "Start: " $start " Range: " $range
+    $range--
     $ping_to = decimal_to_addr -decimal $start
-    write-host $ping_to
+    $up_hosts = ping_range -addr $ping_to -range $range
   }
   else {
+    write-host "Invalid format"
     return
   }
- 
+  $up_hosts |
+  % {
+    write-host $_
+  }
 }
 
 main
