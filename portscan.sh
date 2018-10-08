f676a27a79f8ff69c295a7d1c127dde01096353d
JYCSEC <40046083+JYCSEC@users.noreply.github.com>
2018-10-08 14:56:27 -0400

    port functionality complete

--- portscan.sh
+++ portscan.sh
@@ -1,2 +1,34 @@
+#!/bin/bash
 #Ju Youn Chae
-#Audit Group 6
+if [[ -z $1 || -z $2 ]]; then
+	echo "Usage: <host> <port(s)>"
+	return
+fi
+
+hosts=()
+case $1 in *-*)
+	IFS=- read start end <<< "$1"
+	for ((host=start; host <= end; host++)); do
+		hosts+=($host)
+	done;; *,*)i
+	IFS=, read -ra hosts <<< "$2";;*)
+	hosts+=($1);;
+esac
+	
+ports=()
+case $2 in *-*)
+	IFS=- read start end <<< "$2"
+	for ((port=start; port <= end; port++)); do
+		ports+=($port)
+	done;; *,*)i 
+	IFS=, read -ra ports <<< "$2";;*)
+	ports+=($2);;
+esac
+
+for host in "${hosts[@]}"; do
+	for port in "${ports[@]}"; do
+		timeout ".1" bash -c "echo >/dev/tcp/$host/$port" &&
+			echo "port $port is open" ||
+			echo "port $port is closed"
+	done
+done
